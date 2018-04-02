fa55353d3422e998ab0b4eac0324acf242f23bba
isaac <immartin.bsu@gmail.com>
2018-04-02 00:46:01 -0500

    jquery mouseevents
    added "hover" effect for logo mark to reveal social links.

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,16 +1,40 @@
-//var $navContainer = $('#nav-container');
+$(document).ready(function(){
+	var 	navContent = $('.nav-content'),
+				logoMark = $('#logo-mark'),
+				socialLinks = $('.social'),
+				//logoType = $('#logo-type'),
+				socialContainer = $('#social-container'),
+				content = $('.content-container'),
+				heroTMO = $('#hero-tmo'),
+				tlFadeIn = new TimelineMax();
+				tlMarkHover = new TimelineMax();
 
-//TweenLite.from($navContainer, 1, {width:'0vh'});
+	tlFadeIn
+				.set(socialLinks, {autoAlpha:0})
+				.from(navContent, 1, {autoAlpha:0})
+				//.from(logoMark, 1, {autoAlpha:0}, 0)
+				//.from(socialLinks, 1, {autoAlpha:0}, 1)
+				.from(content, 2.75, {autoAlpha:0, ease: Power1.easeInOut}, 0);
 
-var 	navContent = $('.nav-content'),
-			logoMark = $('#logo-mark'),
-			socialLinks = $('.social'),
-			//logoType = $('#logo-type'),
-			socialContainer = $('#social-container'),
-			tl = new TimelineMax();
+	$(logoMark).mouseenter(
+		function(){
+			tlMarkHover
+					.staggerFromTo(socialLinks, 1,
+						{x:-10, autoAlpha:0},
+						{x:0, autoAlpha:1, ease: Power3.easeOut},
+						0.09);
+		}
+	);
 
-tl
-			.from(navContent, 1, {autoAlpha:0})
-			//.from(logoMark, 1, {autoAlpha:0}, 0)
-			//.from(socialLinks, 1, {autoAlpha:0}, 1)
-			.staggerFrom(socialLinks, 1.25, {x:-10, autoAlpha:0, ease: Power3.easeOut}, 0.07);
+	$(socialContainer).mouseleave(
+		function(){
+			tlMarkHover
+				.staggerFromTo(socialLinks, 0.75,
+					{autoAlpha:1},
+					{autoAlpha:0, ease: Power3.easeOut},
+					0.09);
+			}
+	);
+
+
+});
