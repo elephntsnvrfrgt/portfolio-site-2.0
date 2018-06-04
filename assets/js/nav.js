530a6601a8ab53d7fc1f7c905acfbcd106d03a80
isaac <immartin.bsu@gmail.com>
2018-06-04 00:21:39 -0500

    gsap content reveal + "lazyload" reveal
    Got the TMO content and lazyload reveal with gsap.

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -6,22 +6,38 @@ $(document).ready(function(){
 				socialContainer = $('#social-container'),
 				content = $('.content-container'),
 				heroTMO = $('#hero-tmo'),
+				heroVW = $('#hero-vw'),
+				heroODI = $('#hero-odi'),
+				heroCMHF = $('#hero-cmhf'),
+				descTMO = $('#tmo-description'),
+				descVW = $('#vw-description'),
+				descODI = $('#odi-description'),
+				descCMHF = $('#cmhf-description'),
+				contentTMO = $('#tmo-content'),
+				contentVW = $('#vw-content'),
+				contentODI = $('#odi-content'),
+				contentCMHF = $('#cmhf-content'),
+				junkTMO = ['#tmo-description', '#tmo-content', '#close'],
+				junkVW = ['#vw-description', '#vw-content', '#close'],
+				junkODI = ['#odi-description', '#odi-content', '#close'],
+				junkCMHF = ['#cmhf-description', '#cmhf-content', '#close'],
+				notTMO = ['#hero-vw, #hero-odi, #hero-cmhf'],
+				hidden = $('.hidden'),
 				subContent = $('.subcontent-container'),
-				tlFadeIn = new TimelineMax();
-				tlMarkHover = new TimelineMax();
-				tlSubContent = new TimelineMax();
+				subClose = $('#close'),
+				tlMax = new TimelineMax();
 
-	tlFadeIn
+
+	tlMax
 				.set(socialLinks, {autoAlpha:0})
-				.set('p.reveal.description.span-6', {autoAlpha:0})
 				.from(navContent, 1, {autoAlpha:0})
 				//.from(logoMark, 1, {autoAlpha:0}, 0)
 				//.from(socialLinks, 1, {autoAlpha:0}, 1)
-				.from(content, 2.75, {autoAlpha:0, ease: Power1.easeInOut}, 0);
+				.from(content, 2, {autoAlpha:0, ease: Power1.easeInOut}, 0);
 
 	$(logoMark).mouseenter(
 		function(){
-			tlMarkHover
+			tlMax
 					.staggerFromTo(socialLinks, 1,
 						{x:-10, autoAlpha:0},
 						{x:0, autoAlpha:1, ease: Power3.easeOut},
@@ -31,7 +47,7 @@ $(document).ready(function(){
 
 	$(socialContainer).mouseleave(
 		function(){
-			tlMarkHover
+			tlMax
 				.staggerFromTo(socialLinks, 0.75,
 					{autoAlpha:1},
 					{autoAlpha:0, ease: Power3.easeOut},
@@ -39,6 +55,7 @@ $(document).ready(function(){
 		}
 	);
 
+	//revealing subcontent
 
 	//first attempt
 	// $(heroTMO).click(
@@ -62,18 +79,40 @@ $(document).ready(function(){
 	// 	}
 	// );
 
-	//second attempt
+	//second attempt jquery
+	// $(heroTMO).click(
+	// 	function(){
+	// 		$("#tmo-content, #tmo-description, #close").animate(
+	// 			{
+  //       	opacity: '1',
+	// 				top: '-=40px',
+  //   		},
+	// 			1000
+	// 		);
+	// 	}
+	// );
+
+	//third attempt gsap
 	$(heroTMO).click(
 		function(){
-			$("#tmo-content, #tmo-description, #close").animate(
-				{
-        	opacity: '1',
-					top: '0px',
-    		},
-				1000
-			);
+			tlMax
+					.set(junkTMO, {display:'block'})
+					.staggerTo(junkTMO, 1.25, {y:-20, autoAlpha:1, ease: Power3.easeOut},0.2)
+					//turn other heroes invisible
+					.to(notTMO, 1, {autoAlpha:0, display:'none', ease: Power3.easeOut});
 		}
 	);
+
+	$(subClose).click(
+		function(){
+			tlMax
+					.to(junkTMO, 1,	{autoAlpha:0, ease: Power3.easeOut})
+					//turn other heroes back on
+					.to(notTMO, 1.25, {autoAlpha:1, display:'block', ease: Power3.easeOut})
+					.set(junkTMO, {y:+20, display:'none'});
+		}
+	);
+
 
 
 
@@ -81,6 +120,33 @@ $(document).ready(function(){
 	// $("#hero-vw").click(function(){
   //       $("body" ).load("vw.html .content-container")
   //   });
+
+
+	//scroll reveal
+	var reveals = document.querySelectorAll('.reveal');
+
+	var config = {
+	  threshold: 0.4
+	};
+
+	var observer = new IntersectionObserver(function (entries, self) {
+	  entries.forEach(function (entry) {
+	    if (entry.isIntersecting) {
+	      var overlap = '-=0.3';
+
+	      if (!tlMax.isActive()) {
+	        overlap = '+=0';
+	      }
+
+	      tlMax.fromTo(entry.target, 1.25, {y:60, autoAlpha:0},{y:0, autoAlpha: 1, ease: Power3.easeOut});
+	      self.unobserve(entry.target);
+	    }
+	  });
+	}, config);
+
+	reveals.forEach(function (reveal) {
+	  observer.observe(reveal);
+	});
 
 
 
